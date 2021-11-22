570a4730b12c228e3506b38c79ac275f1a8b369d
roctbb <roctbb@gmail.com>
2021-11-22 14:58:00 +0300

    migration

---
+++ database/migrations/2021_11_22_142644_add_landing_fields_to_course_2.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddLandingFieldsToCourse2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->string('landing_group_size')->nullable();
+            $table->string('landing_price')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('course_2', function (Blueprint $table) {
+            //
+        });
+    }
+}
