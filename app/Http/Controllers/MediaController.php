903bba35e40d8362ac64f95c20907276233fe459
roctbb <roctbb@gmail.com>
2024-11-13 20:11:53 +0300

    Add caching to MediaController responses
    Implemented HTTP caching in the MediaController for media files with a max-age of 3 hours. This should improve performance by reducing unnecessary repeated data transfers.

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -11,21 +11,21 @@ use Storage;
 class MediaController extends Controller
 {
     /**
+     * @param $dir
      * @param $name
      * @return \Illuminate\Http\Response
      */
     public function index($dir, $name)
     {
         $name = $dir.'/'.$name;
-
         if (!Storage::exists($name)) {
             return Response::make('File no found.', 404);
         }
-
         $file = Storage::get($name);
         $type = Storage::mimeType($name);
-        $response = Response::make($file, 200)->header("Content-Type", $type);
-
+        $response = Response::make($file, 200)
+            ->header("Content-Type", $type)
+            ->header("Cache-Control", "public, max-age=" . (3 * 60 * 60)); // кэширование на три часа
         return $response;
     }
 }
