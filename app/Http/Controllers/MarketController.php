fa247804c37980a516295d0c9ac2c2b9086572e7
roctbb <roctbb@gmail.com>
2025-11-03 23:28:10 +0300

    Remove orders

--- app/Http/Controllers/MarketController.php
+++ app/Http/Controllers/MarketController.php
@@ -152,8 +152,8 @@ class MarketController extends Controller
         $order = MarketDeal::findOrFail($id);
 
         $this->make_success_alert("Успех!", 'Доставка товара "' . $order->good->name . '" отменена.', $destination = 'head');
-
-        CoinTransaction::where('user_id', $user->id)->where('comment', 'like', 'Good #' . $order->good->id)->orderBy('id', 'desc')->first()->delete();
+        dd(CoinTransaction::where('user_id', $user->id)->get());
+        CoinTransaction::where('user_id', $user->id)->where('comment', 'like', 'Good #' . $order->good_id)->orderBy('id', 'desc')->first()->delete();
         $order->delete();
 
         return redirect('/insider/market/');
