bfcee828189900811a90a6be8f05a0a6e21d116d
roctbb <roctbb@gmail.com>
2026-04-30 22:01:50 +0300

    Remove unused `Throwable` import in `Handler` class

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,6 @@ namespace App\Exceptions;
 
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
 
 class Handler extends ExceptionHandler
 {
