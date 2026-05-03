305e3855f0ba3ba6c12c2408a0bce2bbc407a522
roctbb <roctbb@gmail.com>
2026-05-03 12:15:20 +0300

    Switch to closure for `addQueryParam` function in `RemoteAuthController`

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
